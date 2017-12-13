2e32f1bf5ad24fe886c3b9bc0cc9d4640c3cde20
Robert Norden <robert.norden@gmx.de>
2017-12-13 17:47:50 +0100

    use the new Core.lua with the new code

---
+++ Core.lua
@@ -0,0 +1,6 @@
+
+---------------------------------
+--      Addon Declaration      --
+---------------------------------
+
+ChaosReserves = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0", "AceDebug-2.0", "AceConsole-2.0")
