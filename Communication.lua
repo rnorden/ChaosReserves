d4b890fb897c9c7eb7d2a454ebaae9f6399e1134
Robert Norden <robert.norden@gmx.de>
2017-12-15 17:26:12 +0100

    reworked Addon, works now with Ace

---
+++ Communication.lua
@@ -0,0 +1,220 @@
+---------------------------------
+-- Communication globals       --
+---------------------------------
+
+ChaosReserves.addonMsgPrefix = "CHAOSRESERVES_"
+ChaosReserves.topic_Leader = "LEADER"
+ChaosReserves.topic_Leader_Request = "REQUEST"
+ChaosReserves.topic_Reservelist = "RESERVELIST"
+ChaosReserves.topic_Reservelist_Request = "REQUEST"
+ChaosReserves.serializationDelimiter = "§"
+ChaosReserves.reserveListSerializationDelimiter = "#"
+
+---------------------------------
+-- Communication functionality --
+---------------------------------
+
+function ChaosReserves:AddonMessage(topic, msg)
+	SendAddonMessage(self.addonMsgPrefix..topic, msg, "GUILD")
+end
+
+function ChaosReserves:GuildMessage(msg)
+	assert(msg, "Message may not be nil")
+	SendChatMessage(msg, "GUILD");
+end
+
+function ChaosReserves:Whisper(recipient, msg)
+	if UnitLevel("player") > 10 then
+		SendChatMessage(msg, "WHISPER", nil, recipient)
+	else
+		self:GuildMessage("@"..recipient..": "..msg)
+	end
+end
+
+function ChaosReserves:GuildMessageTable(msgTable)
+	for _, msg in ipairs(msgTable) do
+		assert(msg, "Message may not be nil")
+		SendChatMessage(msg, "GUILD");
+	end
+end
+
+function ChaosReserves:WhisperOfficersOnly(sender)
+	if sender == UnitName("player") then
+		self:Print("You need to be of rank Lieutenant or higher to do this!")
+	else
+		if UnitLevel("player") < 10 then
+			self:Print(sender.." needs to be of rank Lieutenant or higher to do this!")
+		else
+			self:Whisper(sender, "You need to be of rank Lieutenant or higher to do this!")
+		end
+	end
+end
+
+function ChaosReserves:WhisperChatCommandsHelp(sender)
+	commonCommandHelpTexts = {
+		"add [altname] - add yourself with an optional altname if you're saving buffs",
+		"list - list the current reserve list",
+		"remove - remove yourself",
+		"help - show this help",
+	}
+	officerCommandHelpTexts = {
+		"remove [name] - remove [name] from reserves",
+		"raid [raidname] - set the raid to enable zone checks",
+		"force check - force an afk check",
+		"leader - after sending this you will be the leader",
+	}
+	local prefix = "   "..self.chatCommandPrefix..self.slashCommand1.." "
+	if sender == UnitName("player") then
+		self:Print("Use something like: ")
+		for txt in commonCommandHelpTexts do
+			self:Print(prefix..commonCommandHelpTexts[txt])
+		end
+		if self:IsOfficer(sender) then
+			for txt in officerCommandHelpTexts do
+				self:Print(prefix..officerCommandHelpTexts[txt])
+			end
+		end
+	else
+		self:Whisper(sender, "Use something like: ")
+		for txt in commonCommandHelpTexts do
+			self:Whisper(sender, prefix..commonCommandHelpTexts[txt])
+		end
+		if self:IsOfficer(sender) then
+			for txt in officerCommandHelpTexts do
+				self:Whisper(sender, prefix..officerCommandHelpTexts[txt])
+			end
+		end
+	end
+end
+
+function ChaosReserves:ChatAddonMessageHandler(prefix, message, channel, sender)
+	-- is this message for me?
+	local prefix = string.sub(arg1,1,strlen(self.addonMsgPrefix))
+	local topic = string.sub(arg1,strlen(self.addonMsgPrefix)+1)
+	if  (prefix == self.addonMsgPrefix and 
+		-- do I sneak into the sync or am I legimitately an officer?
+		(self.sneakSync or self:IsOfficer(UnitName("player")))) then
+		self:LevelDebug(2,"Received addon msg on topic ("..topic.."): "..string.sub(message,1,100))
+		if (topic == self.topic_Reservelist) then
+			if (message == self.topic_Reservelist_Request) then
+				self:SendReserveList(sender)
+			else
+				self:ProcessIncomingReserveList(sender, message)
+			end
+		elseif (topic == self.topic_Leader) then
+			if message == self.topic_Leader_Request then
+				self:LevelDebug(2,"Received leader request")
+				self:SendLeader(sender)
+			else
+				self:LevelDebug(2,"Received leader message: "..string.sub(message,1,100))
+				self:SetLeader(sender, message)			
+			end
+		end
+	end
+end
+
+function ChaosReserves:RequestReserveList()
+	self:AddonMessage(self.topic_Reservelist, self.topic_Reservelist_Request)
+end
+
+function ChaosReserves:RequestLeader()
+	self:AddonMessage(self.topic_Leader, self.topic_Leader_Request)
+end
+
+function ChaosReserves:SendLeader()
+	if not self.leader then return; end -- dont reply with empty leader
+	if sender ~= UnitName("player") then -- dont answer your own request
+		self:AddonMessage(self.topic_Leader, self.leader)
+	end
+end
+
+function ChaosReserves:ProcessIncomingLeader(sender, leader)
+	if sender ~= UnitName("player") then -- dont handle your own response
+		self:SetLeader(sender, leader)
+	end
+end
+
+function ChaosReserves:SendReserveList(sender)
+	if not self.reserveList or getn(self.reserveList) == 0 then return; end -- dont reply with empty reserve list
+	if sender ~= UnitName("player") then -- dont answer your own request
+		self:LevelDebug(2,"Incoming reserve list request...")
+		self:AddonMessage(self.topic_Reservelist, self:SerializeReserveList(self.reserveList_Update_Timestamp, self.reserveList))
+		self:LevelDebug(2,"Finished sending my reserve list!")
+	end
+end
+
+function ChaosReserves:ProcessIncomingReserveList(sender, serializedReserveList)
+	if sender ~= UnitName("player") then -- dont handle your own response
+		self:Debug(2,"Incoming reserve list: "..serializedReserveList)
+		local timestamp, reserveList = self:DeserializeReserveList(serializedReserveList)
+		if (tonumber(timestamp) > tonumber(self.reserveList_Update_Timestamp)) then -- the incoming reserveList is newer than what I have
+			self.reserveList_Update_Timestamp = tonumber(timestamp)
+			self.reserveList = reserveList
+			self:Print("Updated reserve list with "..sender.."'s!")
+		end
+	end
+end
+
+function ChaosReserves:SerializeReserveList(timestamp, reserveList)
+	timeStampMap = {}
+	timeStampMap["timestamp"] = timestamp
+	serializedReserveList = self:SerializeMap(timeStampMap) .. self.reserveListSerializationDelimiter
+	if not reserveList then reserveList = { } end
+	for _, reserve in ipairs(reserveList) do
+		serializedReserveList = serializedReserveList .. self:SerializeMap(reserve) .. self.reserveListSerializationDelimiter
+	end
+	return serializedReserveList
+end
+
+function ChaosReserves:DeserializeReserveList(serialize)
+	reservelist = {}
+	self:Debug("Trying to split by "..self.reserveListSerializationDelimiter.." : "..serialize)
+	splitResultList = self:Strsplit(serialize, self.reserveListSerializationDelimiter)
+	local timeStampKeyValuePair = self:DeserializeMap(splitResultList[1])
+	local timestamp = timeStampKeyValuePair["timestamp"]
+	for i=2, getn(splitResultList) do
+		reserve = ChaosReserves:DeserializeMap(splitResultList[i])
+		tinsert(reservelist, reserve)
+	end
+	return timestamp, reservelist
+end
+
+function ChaosReserves:SerializeMap(map)
+	serialize = ""
+	for key, value in pairs(map) do
+		serialize = serialize .. key .. "=" .. value .. self.serializationDelimiter
+	end
+	return serialize
+end
+
+function ChaosReserves:DeserializeMap(serialize)
+	deserializedMap = {}
+	self:Debug("Trying to split by "..self.serializationDelimiter.." : "..serialize)
+	splitResult = self:Strsplit(serialize, self.serializationDelimiter)
+	for i=1, getn(splitResult) do
+		keyValuePair = splitResult[i]
+		self:Debug("Trying to split by ".."=".." : "..keyValuePair)
+		keyValueSplit = self:Strsplit(keyValuePair.."=", "=") --ugly hack, but it doesnt work without the concatenated "="
+		deserializedMap[keyValueSplit[1]] = keyValueSplit[2]
+	end
+	return deserializedMap
+end
+
+function ChaosReserves:Strsplit(pString, pPattern)
+	local Table = {}
+	local fpat = "(.-)" .. pPattern
+	local last_end = 1
+	local s, e, cap = string.find(pString, fpat, 1)
+	while s do
+		if s ~= 1 or cap ~= "" then
+			table.insert(Table,cap)
+		end
+		last_end = e+1
+		s, e, cap = strfind(pString, fpat, last_end)
+	end
+	if last_end <= strlen(pString) then
+		cap = strfind(pString, last_end)
+		table.insert(Table, cap)
+	end
+	return Table
+end
--- Core.lua
+++ Core.lua
@@ -4,3 +4,395 @@
 ---------------------------------
 
 ChaosReserves = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0", "AceDebug-2.0", "AceConsole-2.0")
+ChaosReserves.cmdtable =  {type = "group", handler = ChaosReserves, args = {
+	["enable"] = {
+		type = "execute",
+		name = "enable",
+		desc = "Enable the addon",
+		args = {},
+		func = function() ChaosReserves:OnEnable(); end
+	},
+	["disable"] = {
+		type = "execute",
+		name = "disable",
+		desc = "Disable the addon",
+		args = {},
+		func = function() ChaosReserves:OnDisable(); end
+	},
+	["wipe"] = {
+		type = "execute",
+		name = "wipe",
+		desc = "Wipe the reserve list",
+		args = {},
+		func = function() ChaosReserves:WipeReserves(); end
+	},
+	["list"] = {
+		type = "execute",
+		name = "list",
+		desc = "List the current reserves",
+		args = {},
+		func = function() ChaosReserves:PrintReservesConsole(); end
+	},
+	["guildupdate"] = {
+		type = "execute",
+		name = "guildupdate",
+		desc = "Update Guildroster cache",
+		args = {},
+		func = function() 
+			ChaosReserves:Print("Updating Guildroster cache...")
+			ChaosReserves:UpdateGuildRosterInfoCache()
+			ChaosReserves:Print("Done!") 
+		end
+	},
+	["dumpvars"] = {
+		type = "execute",
+		name = "dumpvars",
+		desc = "Dump variables to the console",
+		args = {},
+		func = function() ChaosReserves:DumpVariables(); end
+	},
+	
+}}
+ChaosReserves.slashCommand1, ChaosReserves.slashCommand2 = "cr", "reserves"
+ChaosReserves:RegisterChatCommand({"/"..ChaosReserves.slashCommand1, "/"..ChaosReserves.slashCommand2}, ChaosReserves.cmdtable)
+
+function ChaosReserves:OnInitialize()
+    self:SetDebugging(true) 
+end
+
+function ChaosReserves:OnEnable()
+    -- Called when the addon is enabled
+	self:RegisterEvent("CHAT_MSG_ADDON")
+	self:RegisterEvent("CHAT_MSG_GUILD")
+	self:RegisterEvent("GUILD_ROSTER_UPDATE")
+	
+	self:RequestReserveList()
+	self:RequestLeader()
+	self:UpdateGuildRosterInfoCache()
+	self:Print("ChaosReserves enabled. Have fun raiding!");
+end
+
+function ChaosReserves:OnDisable()
+    -- Called when the addon is disabled
+end
+
+function ChaosReserves:CHAT_MSG_ADDON(arg1, arg2, arg3, arg4)
+	self:ChatAddonMessageHandler(arg1, arg2, arg3, arg4)
+end
+
+function ChaosReserves:CHAT_MSG_GUILD(arg1, arg2)
+	self:ChatCommandHandler(arg2, arg1)
+end
+
+function ChaosReserves:GUILD_ROSTER_UPDATE()
+	self:UpdateGuildRosterInfoCache()
+end
+
+---------------------------------
+--      Addon Globals          --
+---------------------------------
+
+ChaosReserves.chatCommandPrefix = "#"
+ChaosReserves.lastTimeReservesPrinted = 0
+ChaosReserves.leader = nil
+
+ChaosReserves.raidGates = {
+	["Naxx"] = "Plaguewood",
+	["AQ"] = "Gates of Ahn'Qiraj",
+	["Blackrock"] = "Blackrock Mountain"
+}
+ChaosReserves.doZoneCheck = true
+ChaosReserves.raid = nil
+
+ChaosReserves.reserveList_Update_Timestamp = 0
+ChaosReserves.reserveList = { }
+
+ChaosReserves.lastGuildRosterUpdate = 0
+ChaosReserves.guildRosterInfoCache = { }
+
+---------------------------------
+--   Handle the chat commands  --
+---------------------------------
+
+-- Handle the chat commands prefixed with !reserves
+function ChaosReserves:ChatCommandHandler(sender, msg)
+	self:LevelDebug(3,"ChaosReserves_ChatCommandHandler called with arguments: sender="..sender.." and msg="..msg)
+	local _, _,command, args = string.find(msg, "^"..self.chatCommandPrefix.."(%w+)%s?(.*)");
+	if command then
+		command = strlower(command);
+	else
+		command = "";
+	end
+	if command == ChaosReserves.slashCommand1 or command == ChaosReserves.slashCommand2 then
+		self:LevelDebug(3,"Chatcommand token detected")
+		if (string.find(args, "add%s?")) then
+			_, _, subcommand, altName = string.find(args, "(%w+)%s?(.*)")
+			self:AddReserve(sender, altName)
+		elseif (string.find(args, "remove%s?")) then
+			_, _, subcommand, name = string.find(args, "(%w+)%s?(.*)")
+			if name == "" then name = nil; end
+			self:RemoveReserve(sender, name)
+		elseif (args == "list") then
+			self:PrintReserves()
+		elseif (args == "leader") then
+			self:SetLeader(sender, sender)
+		elseif (string.find(args, "raid%s?")) then
+			_, _, subcommand, raid = string.find(args, "(%w+)%s?(.*)")
+			self:SetRaid(sender, raid)
+		elseif (args == "moinmoin") then
+			if mod(time(),10)==0 then
+				self:GuildMessage("Pucchini is the best! <3")
+			else
+				self:GuildMessage("Moin, "..sender.."!")
+			end
+		else
+			self:WhisperChatCommandsHelp(sender)
+		end
+	end
+end
+
+---------------------------------
+--      Addon Functionality    --
+---------------------------------
+
+function ChaosReserves:ImTheLeader()
+	return self.leader == UnitName("player")
+end
+
+function ChaosReserves:IsOfficer(player)
+	local playerGuildInfo = self.guildRosterInfoCache[player]
+	if playerGuildInfo ~= nil and playerGuildInfo["rankIndex"] < 2 then
+		return true
+	end
+	return false
+end
+
+function ChaosReserves:AddReserve(sender, altName)
+	assert(sender, "Sender may not be null")
+	if self:ImTheLeader() then
+		self:Debug(string.format("I'm the Leader, checking if %s is on reserves", sender))
+		local exists = false
+		if not self.reserveList then self.reserveList = { } end
+		for idx, reserve in ipairs(self.reserveList) do
+			if reserve["name"] == sender then
+				self:Whisper(sender, sender .. " is already on reserves, you idiot!")
+				exists = true
+			end
+		end
+		if not exists then
+			local isPlayerAtGates = self:IsAtRaidGates(sender)
+			if self:IsDebugging() then
+				self:Debug(string.format("%s is not on reserves!", sender))
+				self:LevelDebug(2,string.format("DoZoneCheck? %s; IsAtGates(%s)? %s --> not %s or %s", 
+					tostring(self.doZoneCheck), 
+					sender, 
+					tostring(isPlayerAtGates), 
+					tostring(self.doZoneCheck), 
+					tostring(isPlayerAtGates)
+				))
+			end
+			if not self.doZoneCheck or isPlayerAtGates then
+				local reserve = {}
+				reserve["name"] = sender
+				reserve["timeAdded"] = self:GetGameTime()
+				if altName ~= "" then
+					reserve["altname"] = altName
+				end
+				tinsert(self.reserveList, reserve)
+				self:CallbackReservesUpdated()
+				self:GuildMessage("+")
+			else
+				self:Whisper(sender, "You need to be at the gates ("..self.raidGates[self.raid]..") to be added to reserves!")
+			end
+		else
+			self:Debug(string.format("%s is already on reserves!", sender));
+		end
+	end
+end
+
+function ChaosReserves:RemoveReserve(sender, name)
+	self:LevelDebug(3,"ChaosReserves_RemoveReserve called with arguments: sender="..sender.."; removeName="..tostring(removeName));
+	if self:ImTheLeader() then
+		self:Debug(string.format("I'm the Leader, checking if I am an Officer allowed to remove others", sender))
+		nameToRemove = "" --define variable here for scope...
+		if removeName and not self:IsOfficer(sender) then
+			self:WhisperOfficersOnly(sender)
+			return
+		end
+		if not removeName or removeName == "" then nameToRemove = string.lower(sender) else nameToRemove = string.lower(removeName) end
+		if not self.reserveList then self.reserveList = { } end
+		idxToRemove = 1000
+		for idx, reserve in ipairs(self.reserveList) do
+			reserveName = string.lower(reserve["name"])
+			self:Debug(string.format("Checking if reserve %s == %s", reserveName, nameToRemove))
+			if reserveName == nameToRemove then
+				idxToRemove = idx
+				nameToRemove = reserve["name"] --fix the name for later Guild messages :)
+			end
+		end
+		self:Debug(string.format("Trying to remove index %d from reserveList (%d entries)", idxToRemove, getn(self.reserveList)))
+		if idxToRemove <= getn(self.reserveList) then
+			tremove(self.reserveList, idxToRemove)
+			self:CallbackReservesUpdated()
+			if removeName ~= nil then
+				self:GuildMessage(nameToRemove .. " was removed from reserves by " .. sender .. "!")
+			else
+				self:GuildMessage("+")
+			end
+		end
+	end
+end
+
+function ChaosReserves:SetLeader(sender, newLeader)
+	if newLeader == nil then newLeader = UnitName("player") end
+	if self:IsOfficer(sender) then
+		if self:IsOfficer(newLeader) then
+			if self.leader ~= newLeader then
+				self.leader = newLeader
+				if self:ImTheLeader() then
+					self:GuildMessage("I'm the new leader!")
+					self:AddonMessage(self.topic_Leader, self.leader)
+				end
+			end
+		end
+	else
+		self:WhisperOfficersOnly(sender)
+	end
+end
+
+function ChaosReserves:SetRaid(sender, raid)
+	if self:ImTheLeader() then
+		raidSet = false
+		for availableRaid, gate in pairs(self.raidGates) do
+			if string.lower(availableRaid) == string.lower(raid)  then -- check that requested raid is available
+				self.raid = availableRaid
+				raidSet = true
+			end
+		end
+		if raidSet then
+			self:Print("Raid is now: "..self.raid)
+		else
+			self:Print("Couldn't set the raid. Check that you supplied a valid raid (AQ, Blackrock, Naxx)")
+		end
+	end
+end
+
+function ChaosReserves:WipeReserves()
+	if self:ImTheLeader() then
+		self.reserveList = {}
+		self:GuildMessage("I wiped the reserves list!")
+		self:CallbackReservesUpdated()
+	end
+end
+
+function ChaosReserves:IsAtRaidGates(sender)
+	if not self.raid then self:Print("Can't check if player is at gates because the raid has not been set."); return true; end
+	self:UpdateGuildRosterInfoCache() -- update the guildroster
+	local playerGuildInfo = self.guildRosterInfoCache[player]
+	local raidGate = self.raidGates[ChaosReserves_Raid]
+	local playerIsAtGate = (playerGuildInfo["zone"] == raidGate)
+	self:Debug(string.format("Checking if %s at %s is at raidGate %s --> %s", player, playerGuildInfo["zone"], self.raidGates[self.raid], tostring(playerIsAtGate)))
+	return playerIsAtGate
+end
+
+function ChaosReserves:CallbackReservesUpdated()
+	self.reserveList_Update_Timestamp = time()
+	self:SendReserveList(sender)
+end
+
+function ChaosReserves:UpdateGuildRosterInfoCache()
+	local timeDiff = time() - self.lastGuildRosterUpdate
+	if timeDiff > 10 then
+		SetGuildRosterShowOffline(true) -- include offline guildies
+		GuildRoster()
+		self.lastGuildRosterUpdate = time()
+		self.guildRosterInfoCache = {}
+		for i=1, GetNumGuildMembers() do
+			local name, rank, rankIndex, level, class, zone = GetGuildRosterInfo(i);
+			self.guildRosterInfoCache[name] = {
+				name = name,
+				rank = rank,
+				rankIndex = rankIndex,
+				level = level,
+				class = class,
+				zone = zone
+			}
+		end
+		if self:IsDebugging() then 
+			local idxName = UnitName("player")
+			local me = self.guildRosterInfoCache[idxName]
+			if me then
+				local itemString = "name: "..me["name"].." rank: "..me["rank"].." rankIndex: "..me["rankIndex"].." class: "..me["class"]
+				self:Debug("Example GuildRosterInfoCache item: "..itemString)
+			else
+				self:Debug(self:GetColoredString("ChaosReserves: Couldn't read the guild roster!", "FF0000"))
+			end
+		end
+	end
+end
+
+function ChaosReserves:GetGameTime()
+	local h,m = GetGameTime()
+	local s = self:GetCurrentTimeInUTC()
+	if strlen(h) == 1 then h = "0"..h end
+	if strlen(m) == 1 then m = "0"..m end
+	if strlen(s) == 1 then s = "0"..s end
+	return h..":"..m --..":"..s
+end
+
+---------------------------------------------------------
+--Utility function to calculate the current datetime
+-----------------------------------------------------------
+function ChaosReserves:GetCurrentTimeInUTC()
+	local _days={-1, 30, 58, 89, 119, 150, 180, 211, 242, 272, 303, 333, 364}
+	local _lpdays={}
+	for i=1,2  do _lpdays[i]=_days[i]   end
+	for i=3,13 do _lpdays[i]=_days[i]+1 end
+	local DSEC=24*60*60 -- secs in a day
+	local YSEC=365*DSEC -- secs in a year
+	local LSEC=YSEC+DSEC    -- secs in a leap year
+	local FSEC=4*YSEC+DSEC  -- secs in a 4-year interval
+	local BASE_DOW=4    -- 1970-01-01 was a Thursday
+	local BASE_YEAR=1970    -- 1970 is the base year
+    local y,j,m,d,w,h,n,s
+    local mdays=_days
+    t=time()
+	s=t
+    -- First calculate the number of four-year-interval, so calculation
+    -- of leap year will be simple. Btw, because 2000 IS a leap year and
+    -- 2100 is out of range, this formula is so simple.
+    y=floor(s/FSEC)
+    s=s-y*FSEC
+    y=y*4+BASE_YEAR         -- 1970, 1974, 1978, ...
+    if s>=YSEC then
+        y=y+1           -- 1971, 1975, 1979,...
+        s=s-YSEC
+        if s>=YSEC then
+            y=y+1       -- 1972, 1976, 1980,... (leap years!)
+            s=s-YSEC
+            if s>=LSEC then
+                y=y+1   -- 1971, 1975, 1979,...
+                s=s-LSEC
+            else        -- leap year
+                mdays=_lpdays
+            end
+        end
+    end
+    j=floor(s/DSEC)
+    s=s-j*DSEC
+    local m=1
+    while mdays[m]<j do m=m+1 end
+    m=m-1
+    local d=j-mdays[m]
+    -- Calculate day of week. Sunday is 0
+    w=mod((floor(t/DSEC)+BASE_DOW),7)
+    -- Calculate the time of day from the remaining seconds
+    h=floor(s/3600)
+    s=s-h*3600
+    n=floor(s/60)
+    s=s-n*60
+	
+	--return y.."-"..m.."-"..d.." "..h..":"..n..":"..s
+	--return h..":"..n..":"..s
+	return s
+end
